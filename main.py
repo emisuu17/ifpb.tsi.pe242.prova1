f38542d26cd59020ba4a474fe474e65e553e31b3
emisuu17 <ermesonalvesgit22@gmail.com>
2024-12-13 17:48:49 +0000

    sinc3

--- main.py
+++ main.py
@@ -17,3 +17,26 @@ def q2():
 
 
     pass
+
+
+def q3():
+    """
+    Quantidade de divisores de um número (incluindo 1 e o próprio número)
+    """
+numero = (input('digite um numero:'))
+
+
+pass
+
+def q4():
+
+num1 = int(input('digite um numero:'))
+num2 = int(input('digite um segundo numero:'))
+
+sum
+
+
+pass
+
+ 
+
